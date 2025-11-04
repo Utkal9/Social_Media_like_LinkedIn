02d743973348b69e8e65f6d4e264365aa5081b48
Utkal9 <utkalbehera59@gmail.com>
2025-11-05 02:58:16 +0530

    Creating Layouts, styling UI for getting all posts, all top posts

---
+++ frontend/src/pages/discover/index.jsx
@@ -0,0 +1,24 @@
+import { getAllUsers } from "@/config/redux/action/authAction";
+import DashboardLayout from "@/layout/DashboardLayout";
+import UserLayout from "@/layout/UserLayout";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function Discoverpage() {
+    const authState = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (!authState.all_profiles_fetched) {
+            dispatch(getAllUsers());
+        }
+    }, []);
+    return (
+        <UserLayout>
+            <DashboardLayout>
+                <div>
+                    <h1>Discovers</h1>
+                </div>
+            </DashboardLayout>
+        </UserLayout>
+    );
+}
