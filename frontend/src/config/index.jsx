560e1b08901b8a69880a989174d85b0e3c0d2522
Utkal9 <utkalbehera59@gmail.com>
2025-11-09 19:38:44 +0530

    Changes in backend URL

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:9090";
-
+export const BASE_URL = "https://social-media-0nih.onrender.com";
+// http://localhost:9090
 const clientServer = axios.create({
     baseURL: BASE_URL,
 });
