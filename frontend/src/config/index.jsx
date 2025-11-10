daba42895afb3d41d6a593d698762252007ba8a9
“Utkal9” <utkalbehera59@gmail.com>
2025-11-10 16:33:00 +0530

    Some changes

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const BASE_URL = "https://social-media-0nih.onrender.com";
 // http://localhost:9090
+// https://social-media-0nih.onrender.com
 const clientServer = axios.create({
     baseURL: BASE_URL,
 });
